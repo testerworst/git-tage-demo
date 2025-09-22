d110f5e4b89df2ce9293e2387bd4e1854f563987
subashpernandas <subashpstalin@gmail.com>
2025-09-22 17:48:07 +0530

    add commit for spring boot

---
+++ src/main/java/com/tech/subash/git_tage/GitTageApplication.java
@@ -0,0 +1,13 @@
+package com.tech.subash.git_tage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech/subash/git_tage/GitTageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.subash.git_tage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
